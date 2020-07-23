213e61f788b5642ed4168b0903443869ee7b47a4
lixuan.liu <lixuan.liu@shangri-la.com>
2020-07-23 16:08:08 +0800

    change file mode

--- taskfile.js
+++ taskfile.js
@@ -8,7 +8,7 @@ module.exports = {
         yield task
             .source('packages/xrd/**/*.+(js|jsx|ts|tsx)')
             .babel('server')
-            .target('dist/xrd', {mode: 0o666})
+            .target('dist/xrd', {mode: 777})
         yield task.source('packages/boilerplates/**/*').target('dist/boilerplates')
         notify('Compiled files')
     },
